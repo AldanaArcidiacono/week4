140956c40b7c3a28f261e1939ebc068b86551815
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2022-10-25 15:04:03 +0200

    useEffect

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -2,7 +2,17 @@ import { useEffect, useState } from "react";
 
 export function Counter(data: { data: Array<number> }) {
     const [counter, setCounter] = useState(0);
-    console.log({ data });
+
+    // Este codigo se ejecuta en cada 'render'
+    //console.log({ data });
+
+    // Para que se ejecute solo una vez al principio
+    useEffect(() => {
+        console.log({ data });
+    }, []);
+
+    // Este hook lo usamos para que el codigo de dentro se ejecute sólo
+    // cuando haya un cambio en tus dependecias []
     useEffect(() => {
         console.log(counter);
     }, [counter]);
